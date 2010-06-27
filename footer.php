bde52b8c4808e6471e4904ebd3b136a1d1e91275
bones7456 <bones7456@1520ad4c-5951-11de-8abc-d1c337b90d21>
2010-06-27 07:40:15 +0000

    换域名了，修改主题链接的url

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div id="footer">
 	<p>
 		<span style="font-family:bonesapa,Agency FB, Sans-Serif, Trebuchet MS, Arial;"><?php bloginfo('name'); ?></span> 由 
-		<a href="http://wordpress.org/">WordPress</a> 和 <a href="http://li2z.cn/">bones7456</a> 主题提供支持. <a href="feed:<?php bloginfo('rss2_url'); ?>">订阅文章</a>
+		<a href="http://wordpress.org/">WordPress</a> 和 <a href="http://luy.li/">bones7456</a> 主题提供支持. <a href="feed:<?php bloginfo('rss2_url'); ?>">订阅文章</a>
 		| <a href="feed:<?php bloginfo('comments_rss2_url'); ?>">订阅评论</a>.<br>
 		为了达到最佳浏览效果，请使用firefox等非IE内核浏览器和1024*768以上的分辨率。
 		<a href="<?php bloginfo('url'); ?>/about/beian/">美ICP备98753210号</a>
